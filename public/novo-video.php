0ed43bdd6f632e62761091cc66028f3a034229f9
Will <williango_@hotmail.com>
2024-12-09 09:12:39 -0400

    Will casa

--- public/novo-video.php
+++ public/novo-video.php
@@ -2,13 +2,22 @@
 
 $pdo = new PDO("mysql:host=mariadb;dbname=banquinho", "root", "Psswd#123");
 
-$sql = "INSERT INTO videos (url, title) VALUES (?, ?)";
-$stmt = $pdo->prepare($sql);
-$stmt->bindValue(1, $_POST['url']);
-$stmt->bindValue(2, $_POST['titulo']);
-
-if ($stmt->execute() === false) {
-    header('Location: /index.php?sucesso=0');
-} else {
-    header('Location: /index.php?sucesso=1');
+$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+if ($url === false) {
+    header('Location: /?sucesso=0');
+    exit();
 }
+$titulo = filter_input(INPUT_POST, 'titulo', FILTER_SANITIZE_EMAIL);
+if ($titulo === false) {
+    header('Location: /?sucesso=0');
+    exit();
+}
+
+$sql = 'INSERT INTO videos (url, title) VALUES (?, ?);';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(1, $_POST["url"]);
+$stmt->bindValue(2, $_POST["titulo"]);
+$stmt->execute();
+
+
+header('Location: /?sucesso=1');
